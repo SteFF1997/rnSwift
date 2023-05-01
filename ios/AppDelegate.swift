e7c8b78fd083511e0f0069cd542424839666a1d0
Ivan Hassay <ivan.hassay@myzeller.com>
2023-05-01 18:59:17 +0300

    init

---
+++ ios/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  RNSwiftKotlin
+//
+//  Created by MacBook on 01.05.2023.
+//
+
+import Foundation
+@UIApplicationMain
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+  var bridge: RCTBridge!
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+  
+    let jsCodeLocation: URL
+      jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    
+    let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "RNSwiftKotlin", initialProperties: nil, launchOptions: launchOptions)
+    
+    let rootViewController = UIViewController()
+    rootViewController.view = rootView
+
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    self.window?.rootViewController = rootViewController
+    self.window?.makeKeyAndVisible()
+    
+    return true
+  }
+}
